2151ad7b42769b273858f2a6517a1028b95f97e6
Sachin Saini <iamsainisachin@gmail.com>
2019-01-20 01:16:54 +0530

    add smart contract

---
+++ contracts/Inbox.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.25;
+
+contract Inbox{
+	string public message;
+	constructor(string memory initialMessage) public {
+		message = initialMessage;
+	}
+
+	function setMessage(string memory newMessage) public {
+		message = newMessage;
+	}
+}
